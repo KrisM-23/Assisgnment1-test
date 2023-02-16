79f2e53a25cc61a1cea172edf054cf5af8e78813
Kris <krismendoza445@gmail.com>
2023-02-16 14:54:36 -0800

    Initial Commit

---
+++ Assisgnment1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assisgnment1
+//
+//  Created by Kris Mendoza on 2/16/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
